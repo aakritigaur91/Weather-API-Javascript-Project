dc146ab32b8cf67c49e386ef1231b91f3f29432d
aakritigaur91 <2k23.csiot2310885@gmail.com>
2025-08-21 15:52:50 +0530

    Updated js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const cityInput = document.querySelector('.city-input')
 const searchBtn = document.querySelector('.search-btn')
 
+const weatherInfoSection = document.querySelector('.weather-info')
+const notFoundSection = document.querySelector('.not-found')
+const searchCitySection = document.querySelector('.search-city')
+
 const apiKey = '2f5cbb783c260fb0a4e420737af7a0fe'
 
 searchBtn.addEventListener('click', () => {
@@ -20,12 +24,20 @@ cityInput.addEventListener('keydown', (event) => {
 })
 
 async function getFetchData(endPoint, city) {
-    const apiUrl = 'https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}'
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`
     const response = await fetch(apiUrl)
     return response.json()
 }
 
 async function updateWeatherInfo(city) {
     const weatherData = await getFetchData('weather', city)
+    if (weatherData.cod != 200) {
+        showDisplaySection(notFoundSection)
+        return
+    }
     console.log(weatherData)
+}
+
+function showDisplaySection(section) {
+    
 }
