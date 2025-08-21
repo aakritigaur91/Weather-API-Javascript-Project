fcbb5a9c52a89a7d46dfb44a5fbbc7e0757c2199
aakritigaur91 <2k23.csiot2310885@gmail.com>
2025-08-21 10:35:04 +0530

    Updated js

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 const cityInput = document.querySelector('.city-input')
 const searchBtn = document.querySelector('.search-btn')
 
+const apiKey = '2f5cbb783c260fb0a4e420737af7a0fe'
+
 searchBtn.addEventListener('click', () => {
     if (cityInput.value.trim != '') {
-        console.log(cityInput.value)
+        updateWeatherInfo(cityInput.value)
         cityInput.value = ''
         cityInput.blur()
     }
 })
+cityInput.addEventListener('keydown', (event) => {
+    if(event.key == 'Enter' && cityInput.value.trim != '') {
+        updateWeatherInfo(cityInput.value)
+        cityInput.value = ''
+        cityInput.blur()
+    }
+    console.log(event)
+})
+
+async function getFetchData(endPoint, city) {
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}'
+    const response = await fetch(apiUrl)
+    return response.json()
+}
+
+async function updateWeatherInfo(city) {
+    const weatherData = await getFetchData('weather', city)
+    console.log(weatherData)
+}
